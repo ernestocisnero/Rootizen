1223dbf19bc73d0063658456c0f89e01196585b1
Ernesto Cisnero <miguelernestocisnero@gmail.com>
2026-08-20 23:50:17 +0200

    Initial Commit

---
+++ Unionify/UnionifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnionifyApp.swift
+//  Unionify
+//
+//  Created by Ernesto Cisnero on 8/20/26.
+//
+
+import SwiftUI
+
+@main
+struct UnionifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
